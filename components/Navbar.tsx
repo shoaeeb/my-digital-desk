8cf8c8758cc7777641a41afeccd4b9ee7295d9f3
shoaeeb <osman.shoaeeb@gmail.com>
2026-01-09 19:54:28 +0530

    Added signout button and added profilepic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,13 @@ import { LogoSvg } from "../app/constants/constants";
 import styles from "./navbar.module.css";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const Navbar = () => {
   const router = useRouter();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { data: session, status } = useSession();
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>
@@ -27,17 +30,46 @@ const Navbar = () => {
             About
           </Link>
         </div>
+        {status !== "loading" && status == "authenticated" && (
+          <div className={styles.profile}>
+            <div>
+              <h3>{session.user?.name}</h3>
+              <p className={styles.emailAddress}> {session.user?.email}</p>
+            </div>
+            <Image
+              className={styles.profilePic}
+              src={session.user?.image!}
+              alt="User Profile Pic"
+              height={40}
+              width={40}
+            />
+          </div>
+        )}
         <div className={styles.buttons}>
-          <button className={styles.getStarted}>Get Started</button>
-          <button
-            onClick={() => {
-              console.log("clicked");
-              router.push("/login");
-            }}
-            className={styles.signIn}
-          >
-            Sign In
-          </button>
+          {status !== "loading" && status === "unauthenticated" && (
+            <button className={styles.getStarted}>Get Started</button>
+          )}
+          {status !== "loading" && status == "unauthenticated" && (
+            <button
+              onClick={() => {
+                console.log("clicked");
+                router.push("/login");
+              }}
+              className={styles.signIn}
+            >
+              Sign In
+            </button>
+          )}
+          {status !== "loading" && status === "authenticated" && (
+            <button
+              onClick={() => {
+                signOut({ callbackUrl: "/" });
+              }}
+              className={styles.signIn}
+            >
+              Sign Out
+            </button>
+          )}
         </div>
       </div>
       {/* mobile Actions */}
@@ -58,16 +90,45 @@ const Navbar = () => {
           <Link className={styles.mobileLink} href={"/about"}>
             About
           </Link>
-          <button
-            onClick={() => {
-              console.log("clicked");
-              router.push("/login");
-            }}
-            className={styles.mobileSignIn}
-          >
-            Sign In
-          </button>
-          <button className={styles.mobileGetStarted}>Get Started</button>
+          {status !== "loading" && status == "authenticated" && (
+            <div className={styles.profile}>
+              <div>
+                <h3>{session.user?.name}</h3>
+                <p className={styles.emailAddress}> {session.user?.email}</p>
+              </div>
+              <Image
+                className={styles.profilePic}
+                src={session.user?.image!}
+                alt="User Profile Pic"
+                height={40}
+                width={40}
+              />
+            </div>
+          )}
+          {status !== "loading" && status === "unauthenticated" && (
+            <button
+              onClick={() => {
+                console.log("clicked");
+                router.push("/login");
+              }}
+              className={styles.mobileSignIn}
+            >
+              Sign In
+            </button>
+          )}
+          {status !== "loading" && status === "authenticated" && (
+            <button
+              onClick={() => {
+                signOut({ callbackUrl: "/" });
+              }}
+              className={styles.mobileSignIn}
+            >
+              Signout
+            </button>
+          )}
+          {status !== "loading" && status === "unauthenticated" && (
+            <button className={styles.mobileGetStarted}>Get Started</button>
+          )}
         </div>
       )}
     </div>
