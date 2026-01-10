a73a42d9f8ed5c0dbfd76ce1e9b588dd56d66f0e
shoaeeb <osman.shoaeeb@gmail.com>
2026-01-10 19:46:30 +0530

    Added Sidebar and added modal

--- app/dashboard/SidebarToggle.tsx
+++ app/dashboard/SidebarToggle.tsx
@@ -76,60 +76,99 @@ const categories = [
 function SidebarToggle({ children }: SidebarToggleProps) {
   const [sidebaropen, setSidebarOpen] = useState(true);
   const [createCategoryModal, setCreateCategoryModal] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(CATEGORY_COLORS[0]);
   return (
-    <div className={styles.mainContainer}>
-      <div
-        className={`${styles.sidebar} ${
-          !sidebaropen ? styles.sidebarClosed : ""
-        }`}
-      >
-        <button
-          className={styles.toggleButton}
-          onClick={() => setSidebarOpen(!sidebaropen)}
-        >
-          {sidebaropen ? (
-            <HiX className={styles.icon} />
-          ) : (
-            <HiMenuAlt3 className={styles.icon} />
-          )}
-        </button>
-        {/* TODO Sidebar */}
-        {sidebaropen && (
-          <div className={styles.sidebarContent}>
-            <h3 className={styles.categoryText}>Categories</h3>
-            {categories.map((category: CategoryType) => {
-              return (
-                <div
-                  style={{ backgroundColor: category.color }}
-                  className={styles.categoryItem}
-                  key={category._id}
-                >
-                  {category.name}
-                </div>
-              );
-            })}
-            <button className={styles.createCategoryBtn}>
-              <IoIosAddCircle size={20} className={styles.createIcon} />
-              Add Category
-            </button>
-          </div>
-        )}
-      </div>
-      <div className={styles.noteContainer}>
-        <h1>All Notes</h1>
-        <p className={styles.subheading}>
-          Manage your personal knowledge base.
-        </p>
-
+    <>
+      <div className={styles.mainContainer}>
         <div
-          className={`${styles.emptyState} ${
-            !sidebaropen ? styles.emptyStateExpanded : ""
+          className={`${styles.sidebar} ${
+            !sidebaropen ? styles.sidebarClosed : ""
           }`}
         >
-          {children}
+          <button
+            className={styles.toggleButton}
+            onClick={() => setSidebarOpen(!sidebaropen)}
+          >
+            {sidebaropen ? (
+              <HiX className={styles.icon} />
+            ) : (
+              <HiMenuAlt3 className={styles.icon} />
+            )}
+          </button>
+          {/* TODO Sidebar */}
+          {sidebaropen && (
+            <div className={styles.sidebarContent}>
+              <h3 className={styles.categoryText}>Categories</h3>
+              {categories.map((category: CategoryType) => {
+                return (
+                  <div
+                    style={{ backgroundColor: category.color }}
+                    className={styles.categoryItem}
+                    key={category._id}
+                  >
+                    {category.name}
+                  </div>
+                );
+              })}
+              <button
+                onClick={() => setCreateCategoryModal(true)}
+                className={styles.createCategoryBtn}
+              >
+                <IoIosAddCircle size={20} className={styles.createIcon} />
+                Add Category
+              </button>
+            </div>
+          )}
+        </div>
+        <div className={styles.noteContainer}>
+          <h1>All Notes</h1>
+          <p className={styles.subheading}>
+            Manage your personal knowledge base.
+          </p>
+
+          <div
+            className={`${styles.emptyState} ${
+              !sidebaropen ? styles.emptyStateExpanded : ""
+            }`}
+          >
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+      {createCategoryModal && (
+        <div className={"modalOverlay"}>
+          <div className={"modalContent"}>
+            <h2>Create New Category</h2>
+            <input
+              type="text"
+              placeholder="Category name"
+              className={"modalInput"}
+            />
+            <div className={"colorPicker"}>
+              {CATEGORY_COLORS.map((color) => (
+                <div
+                  onClick={() => setSelectedColor(color)}
+                  style={{ backgroundColor: color }}
+                  className={`colorOption ${
+                    selectedColor === color ? "selectedColor" : ""
+                  }`}
+                  key={color}
+                ></div>
+              ))}
+            </div>
+            <div className={"modalActions"}>
+              <button
+                className={"cancelBtn"}
+                onClick={() => setCreateCategoryModal(false)}
+              >
+                Cancel
+              </button>
+              <button className={"createBtn"}>Create</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
